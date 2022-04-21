06dbf148bc3920a59d9b00ab38a93f4a92d60a97
Maycon0001 <101477010+Maycon0001@users.noreply.github.com>
2022-04-21 16:49:22 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#flexSwitchCheckDefault')
-
-$checkbox.addEventListener('change', function(){
-    $html.classList.toggle('dark-mode')
-})
